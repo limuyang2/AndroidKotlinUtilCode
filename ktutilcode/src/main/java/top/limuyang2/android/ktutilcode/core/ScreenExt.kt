ede055357b82c7367d6c2e3c3b349be854f13b2d
limuyang <limuyang2@163.com>
2019-01-25 14:28:55 +0800

    完善屏幕相关扩展

--- ktutilcode/src/main/java/top/limuyang2/android/ktutilcode/core/ScreenExt.kt
+++ ktutilcode/src/main/java/top/limuyang2/android/ktutilcode/core/ScreenExt.kt
@@ -1,12 +1,19 @@
 package top.limuyang2.android.ktutilcode.core
 
+import android.Manifest.permission.WRITE_SETTINGS
 import android.app.Activity
+import android.app.KeyguardManager
 import android.content.Context
+import android.content.pm.ActivityInfo
+import android.content.res.Configuration
 import android.graphics.Bitmap
 import android.graphics.Point
 import android.os.Build
+import android.provider.Settings
+import android.support.annotation.RequiresPermission
 import android.util.DisplayMetrics
 import android.view.WindowManager
+
 
 /**
  * 获取屏幕宽度
@@ -45,7 +52,7 @@ inline val Context.screenHeight: Int
     }
 
 /**
- * Set full screen.
+ * 全屏
  */
 inline var Activity.fullScreen: Boolean
     set(value) {
@@ -61,7 +68,73 @@ inline var Activity.fullScreen: Boolean
     }
 
 /**
- * Return the bitmap of screen.
+ * 是否横屏
+ */
+inline var Activity.landscape: Boolean
+    set(value) {
+        if (value) {
+            this.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        } else {
+            this.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        }
+    }
+    get() {
+        return this.resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
+    }
+
+/**
+ * 是否竖屏
+ */
+inline var Activity.portrait: Boolean
+    set(value) {
+        if (value) {
+            this.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        } else {
+            this.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        }
+    }
+    get() {
+        return this.resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT
+    }
+
+/**
+ * 判断是否锁屏
+ */
+inline val Context.isScreenLock: Boolean
+    get() {
+        val km = this.getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
+        //noinspection ConstantConditions
+        return km.inKeyguardRestrictedInputMode()
+    }
+
+/**
+ * 进入休眠时长
+ */
+inline var Context.sleepDuration: Int
+    @RequiresPermission(WRITE_SETTINGS)
+    set(value) {
+        Settings.System.putInt(
+                this.contentResolver,
+                Settings.System.SCREEN_OFF_TIMEOUT,
+                value
+        )
+    }
+    get() {
+        return try {
+            Settings.System.getInt(
+                    this.contentResolver,
+                    Settings.System.SCREEN_OFF_TIMEOUT
+            )
+        } catch (e: Settings.SettingNotFoundException) {
+            e.printStackTrace()
+            -123
+        }
+    }
+
+
+
+/**
+ * 截屏
  *
  * @param isDeleteStatusBar True to delete status bar, false otherwise.
  * @return the bitmap of screen
